c81037bb7b8f12486589a38e591d007a79f606a5
barackChen <barackchen@outlook.com>
2019-07-20 00:15:01 +0800

    Update source caused by the merge issue

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ class BaseConfig(object):
     NNN = 123
     FILE_PATH = 'C:\\Users\\Administrator\\PycharmProjects\\File\\'
     MODEL_PATH = 'saved_models/Emotion_Voice_Detection_Model.h5'
+    DB_PATH = 'mysql://root:12345678@sentiment.cepghepnnhzt.ap-northeast-1.rds.amazonaws.com:3306/sentiment'
 
 class TestConfig(BaseConfig):
     DB = '127.0.0.1'
